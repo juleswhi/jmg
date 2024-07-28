98fe500419859d537e24d82938b7f18df765ef73
Jules White <juleswhite@127.0.0.1>
2024-07-28 22:22:42 +0100

    Add .gitignore

--- ytdl.py
+++ ytdl.py
@@ -1,8 +1,9 @@
+import time
 import os
 import subprocess
 
 DOWNLOAD_FILE = "download.txt"
-FULL_DOWNLOAD_FILE = "download_full.tmp"
+FULL_DOWNLOAD_FILE = "download_full.txt"
 OUTPUT_DIR = "download_output"
 
 DEVNULL = open(os.devnull, 'wb')
@@ -15,25 +16,39 @@ total_len = lines.__len__()
 
 fdf = open(FULL_DOWNLOAD_FILE, "w")
 
-for line in lines:
+start_time = time.time()
+
+for line in set(lines):
     if "playlist" in line:
-        _ = subprocess.run(["yt-dlp", "--flat-playlist", "-i", "--print-to-file", "url", FULL_DOWNLOAD_FILE, line])
-        print(f"Decompressed Playlist: {line}")
-        fdf.write("\n")
+        _ = subprocess.run(["yt-dlp", "--flat-playlist", "-i", "--print-to-file", "url", FULL_DOWNLOAD_FILE, line], stdout=DEVNULL, stderr=DEVNULL)
+        print(f"Decompressed Playlist: {line.strip()}")
     else:
-        fdf.write(f"{line}\n")
+        fdf.write(f"{line}")
 
-lines = fdf.readlines()
+end_time = time.time()
+print(f"Time elapsed for playlist conversion: {end_time - start_time}")
+fdf.close()
 
+downloads = open(FULL_DOWNLOAD_FILE, "r")
+lines = downloads.readlines()
+
+total_len = lines.__len__()
+
+total_elapsed_time = 0
+estimated_elapsed = 0
+average_elapsed = 0
 
 for line in lines:
+    start = time.time()
     command = f'yt-dlp -o "{OUTPUT_DIR}/%(title)s.%(ext)s" {line.strip()}'
-    _ = subprocess.run(command, shell=True, stdout=DEVNULL)
-    print("*********************")
-    print("")
-    print(f"Complete: {i} / {total_len}, {(i / total_len) * 100}%")
-    print("")
-    print("*********************")
-    i = i + 1
+    _ = subprocess.run(command, shell=True, stdout=DEVNULL, stderr=DEVNULL)
+    end = time.time()
+    if average_elapsed == 0:
+        average_elapsed = (end - start)
+    else:
+        average_elapsed = (average_elapsed + (end - start) ) / 2
 
-print("DONE!")
+    i = i + 1
+    print(f"Complete: {i} / {total_len}, {(i / total_len) * 100}%   |   Elapsed: {end - start}   |   Estimated Time Left: {(total_len - i) * average_elapsed}")
+
+print(f"Finished!\n.Mp4 files are located at: {OUTPUT_DIR}")
