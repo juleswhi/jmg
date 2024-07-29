df8c88581bb7de843225712b0baeeb7cd37e18df
Jules White <juleswhite@127.0.0.1>
2024-07-30 00:07:47 +0100

    Update ffmpeg.py to use multithreading

--- ffmpeg.py
+++ ffmpeg.py
@@ -2,11 +2,9 @@ import time
 import os
 import subprocess
 import glob
-
-# Define the directory containing the MP4 files
-directory_path = 'download_output'
-
+from concurrent.futures import ThreadPoolExecutor
 import math
+
 def truncate(number, digits) -> float:
     nbDecimals = len(str(number).split('.')[1])
     if nbDecimals <= digits:
@@ -14,25 +12,29 @@ def truncate(number, digits) -> float:
     stepper = 10.0 ** digits
     return math.trunc(stepper * number) / stepper
 
+directory_path = 'download_output'
+
 files = glob.glob('output/*')
 for f in files:
     os.remove(f)
 
-i = 1
-total = files.count
 DEVNULL = open(os.devnull, 'wb')
-for filename in os.listdir(directory_path):
-    if filename.endswith('.mp4'):
-        start = time.time()
-        mp4_file_path = os.path.join(directory_path, filename)
 
-        mp3_filename = filename.split(".mp4")[0] + ".mp3"
-        mp3_file_path = os.path.join("output", mp3_filename)
-        ffmpeg_command = f'ffmpeg -i "{mp4_file_path}" -vn -acodec libmp3lame -q:a 2 "{mp3_file_path}"'
+counter = [0]
+total = 1500
 
-        _ = subprocess.run(ffmpeg_command, shell=True, stdout=DEVNULL,stderr=DEVNULL)
-        i = i + 1
-        end = time.time()
-        print(f"Processed: {i} / {total}  |  {(i / total) * 100}%  |  Time Taken: {truncate(end - start, 2)}  |  Saved: {mp3_file_path}")
+def convert_mp4_to_mp3(mp4_file_path):
+    filename = os.path.basename(mp4_file_path)
+    mp3_filename = filename.split(".mp4")[0] + ".mp3"
+    mp3_file_path = os.path.join("output", mp3_filename)
+    ffmpeg_command = f'ffmpeg -i "{mp4_file_path}" -vn -acodec libmp3lame -q:a 2 "{mp3_file_path}"'
+    subprocess.run(ffmpeg_command, shell=True, stdout=DEVNULL, stderr=DEVNULL)
+    counter[0] += 1
+    print(f"completed: {truncate((counter[0] / total) * 100, 1)}%")
+
+with ThreadPoolExecutor(max_workers=10) as executor:
+    mp4_files = glob.glob(os.path.join(directory_path, '*.mp4'))
+    executor.map(convert_mp4_to_mp3, mp4_files)
 
 print("Conversion completed.")
+
